216d9841ad5807f41f39fb8ce424ffff718c3b3c
Vaibhav Gupta <56396253+guptajihere@users.noreply.github.com>
2021-06-07 17:15:46 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.chat_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText phone;
+    EditText message;
+    Button send;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        phone=findViewById(R.id.PhoneEt);
+        message=findViewById(R.id.MessageEt);
+        send=findViewById(R.id.button);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String number=phone.getText().toString();  // This gets the number that we type in editText,converts it into a string and uses it in the next step
+                String msg=message.getText().toString();
+
+                String numberURL="https://api.whatsapp.com/send?phone=91"+ number +"&text=" +msg;  // "&text" is another url
+
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(numberURL)); // intent is used to change application or an activity on press of a button
+               // ACTION_SEND intent is used as we want to send a Uri from one application to another
+            startActivity(intent);
+            //setTitle("Home chat");
+
+            }
+        });
+
+    }
+}
